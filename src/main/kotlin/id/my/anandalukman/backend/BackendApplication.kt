a456caaa53e3d7502f6d7b14d7248896d94c42ce
Ananda Lukman <ananda.muhamad.lukman@gmail.com>
2022-01-15 17:21:54 +0700

    Setup Installation Spring Boot base on Kotlin

---
+++ src/main/kotlin/id/my/anandalukman/backend/BackendApplication.kt
@@ -0,0 +1,11 @@
+package id.my.anandalukman.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendApplication>(*args)
+}
